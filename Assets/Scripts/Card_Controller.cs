cfb510a379317f3383fdbef8ffe09908c8a7ff24
ZorxExt <franciscosabatecastillo@gmail.com>
2023-01-16 23:01:46 -0300

    cosas

--- Assets/Scripts/Card_Controller.cs
+++ Assets/Scripts/Card_Controller.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 
 public class Card_Controller : MonoBehaviour
 {
-    public TextMeshPro color;
+    public int color_input;
+    public int number_input;
     public TextMeshPro number;
+
+    public Material red_card;
+    public Material blue_card;
+    public Material yellow_card;
+    public Material green_card;
     void Start()
     {
-        
+        switch (color_input)
+        {
+            case 1:
+                this.GetComponent<Renderer>().material = red_card;
+                break;
+            case 2:
+                this.GetComponent<Renderer>().material = blue_card;
+                break;
+            case 3:
+                this.GetComponent<Renderer>().material = yellow_card;
+                break;
+            case 4:
+                this.GetComponent<Renderer>().material = green_card;
+                break;
+        }
+        number.text = number_input.ToString();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Cards_Master.cs
+++ Assets/Scripts/Cards_Master.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class Cards_Master : MonoBehaviour
 {
-    public Vector2 player_card1;
-    public Vector2 player_card2;
-    public Vector2 player_card3;
-    public object vanilla_card;
+    public Dictionary<string, Vector2Int> card_deck = new Dictionary<string, Vector2Int>();
+
+    public Vector2Int player_card1b;
+    public Vector2Int player_card2b;
+    public Vector2Int player_card3b;
+    public GameObject vanilla_card;
+    private GameObject _player_card1;
+    private GameObject _player_card2;
+    private GameObject _player_card3;
+
     void Start()
     {
-        player_card1.x = Random.Range(1, 4);
-        player_card1.y = Random.Range(1, 12);
-        player_card2.x = Random.Range(1, 4);
-        player_card2.y = Random.Range(1, 12);
-        player_card3.x = Random.Range(1, 4);
-        player_card3.y = Random.Range(1, 12);
+        Shuffle_player();
     }
 
-
-    void Update()
+    public void Create_Deck()
     {
-        
+        for (int x = 1; x < 5; x++)
+        {
+            for (int i = 1; i < 13; i++)
+            {
+                card_deck.Add();
+            }
+        }
     }
+    
+
+    public void Shuffle_player()
+    {
+        player_card1b.x = Random.Range(1, 4);
+        player_card1b.y = Random.Range(1, 12);
+        player_card2b.x = Random.Range(1, 4);
+        player_card2b.y = Random.Range(1, 12);
+        player_card3b.x = Random.Range(1, 4);
+        player_card3b.y = Random.Range(1, 12);
+
+        _player_card1 = Instantiate(vanilla_card, new Vector3(0, 0, 0), Quaternion.identity);
+        _player_card1.GetComponent<Card_Controller>().color_input = player_card1b.x;
+        _player_card1.GetComponent<Card_Controller>().number_input = player_card1b.y;
+
+        _player_card2 = Instantiate(vanilla_card, new Vector3(0, 0, 1.5f), Quaternion.identity);
+        _player_card2.GetComponent<Card_Controller>().color_input = player_card2b.x;
+        _player_card2.GetComponent<Card_Controller>().number_input = player_card2b.y;
+
+        _player_card3 = Instantiate(vanilla_card, new Vector3(0, 0, -1.5f), Quaternion.identity);
+        _player_card3.GetComponent<Card_Controller>().color_input = player_card3b.x;
+        _player_card3.GetComponent<Card_Controller>().number_input = player_card3b.y;
+    }
+    
+    
+    
 }
